19b4737d7b0b5b580403157117456c8d5fc25a28
elmerdigital12 <elmer.wipe@gmail.com>
2025-10-17 19:50:33 +0200

    buttononwebsite

--- app.js
+++ app.js
@@ -2,41 +2,33 @@ document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById('showTimeBtn');
     const display = document.getElementById('timeDisplay');
     let intervalId = null;
-
-
-
-
     function updateTime() {
         const now = new Date();
-
-
         const options = {
-            timeZone: 'Europe/Stockholm', // ska vara gmt +2
+            timeZone: 'Europe/Stockholm', // ska vara gmt +2 //, kan sen lägga till datum för ändring
             hour: '2-digit',
             minute: '2-digit',
             second: '2-digit',
             hour12: false,
         };
-
-
         const timeInGMT2 = new Intl.DateTimeFormat('en-GB', options).format(now);
         display.textContent = `Current time (GMT+2): ${timeInGMT2}`;
     }
-
-    button.addeventListener('click', () => {
+    button.addEventListener('click', () => {
         if (intervalId === null) {
             updateTime();
             intervalId = setInterval(updateTime, 1000);
-            button.textContent = 'Stop Clock';
+            button.textContent = 'Stop';
         } else {
             clearInterval(intervalId);
             intervalId = null;
-            button.textContent = 'Show Current Time (GMT+2)';
+            button.textContent = 'Display Current Time Stockholm (GMT+2)';
             display.textContent = '';
 
         }
-    });
+    })
 });
+// app js = interaktivitet
 
 
 
